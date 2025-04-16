8c2547c65e1e51fa92964ef80af169fed1682469
Paulo972021 <84360529+Paulo972021@users.noreply.github.com>
2025-04-16 19:02:41 -0300

    Update etl_indicadores_economicos.py

--- ETL/etl_indicadores_economicos.py
+++ ETL/etl_indicadores_economicos.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import isnull, isnan, col, count, when
+from pyspark.sql.functions import isnull, isnan, col, count, when, udf
 from pyspark.sql.types import *
 from pyspark.sql.types import StringType
 import pycountry
