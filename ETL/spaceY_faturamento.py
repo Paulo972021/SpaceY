f170432683923e62573691bd76179670877899f1
Paulo972021 <84360529+Paulo972021@users.noreply.github.com>
2025-04-16 19:05:56 -0400

    Update spaceY_faturamento.py

--- ETL/spaceY_faturamento.py
+++ ETL/spaceY_faturamento.py
@@ -1,4 +1,5 @@
 from pyspark.sql.functions import *
+from pyspark.sql.functions import trim
 from pyspark.sql.types import *
 from pyspark.sql.types import StringType
 from pyspark.sql.types import TimestampType
