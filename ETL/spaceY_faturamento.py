1c8a68f593a90cfe0dff0cb2953f21c28f59d82b
Paulo972021 <84360529+Paulo972021@users.noreply.github.com>
2025-04-16 19:04:13 -0400

    Update spaceY_faturamento.py

--- ETL/spaceY_faturamento.py
+++ ETL/spaceY_faturamento.py
@@ -1,5 +1,5 @@
 from pyspark.sql.functions import isnull, isnan, col, count, when
-from pyspark.sql.functions import lower
+from pyspark.sql.functions import lower, trim
 from pyspark.sql.types import *
 from pyspark.sql.types import StringType
 from pyspark.sql.types import TimestampType
